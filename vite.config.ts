cb8172b5cb5e88464b906308f1e9942e070b23ff
Fernanda H. Passon <nandahaese@gmail.com>
2026-01-23 17:55:51 -0300

    Alterações em package.json e vite.config.ts para testar deploy por gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ import yaml from "@rollup/plugin-yaml";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "/sensen-games/",
+  server: {
+    port: 8080,
+    host: "::",
+  },
   plugins: [
     react(),
     yaml(),
