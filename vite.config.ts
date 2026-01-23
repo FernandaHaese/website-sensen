a0acf7dcdc73e1418d88cf97f8d1e717ed1566b2
Fernanda H. Passon <nandahaese@gmail.com>
2026-01-23 15:55:20 -0300

    Deploy com gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import yaml from '@rollup/plugin-yaml';
+import yaml from "@rollup/plugin-yaml";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -10,10 +10,11 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: "/sensen-games",
   plugins: [
-    react(), 
+    react(),
     yaml(),
-    mode === "development" && componentTagger()
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
